95503698c4a53cdd61f9f56ded3244c5b328e171
Jake Krueger <jmk4u14@gmail.com>
2021-03-15 02:29:47 -0400

    Added date and exercise data to workout.js

---
+++ Develop/models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workout = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            exerciseType: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise type."
+            },
+            exerciseName: {
+                type: String,
+                trim: true,
+                required: "Please enter the name of the exercise."
+            },
+            distance: {
+                type: Number,
+                required: "Please enter how long you traveled."
+            },
+            time: {
+                type: Number,
+                required: "Please enter the exercise duration."
+            },
+            weight: {
+                type: Number,
+                required: "Please enter the total number of weights used."
+            },
+            reps: {
+                type: Number,
+                required: "Please enter the amount of reps you completed."
+            },
+            sets: {
+                type: Number,
+                required: "Please enter the amount of sets you completed."
+            },
+        },
+    ],
+});
