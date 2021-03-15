ac826ef10770f21f8ebe205a0610e2d35d4a185a
Jake Krueger <jmk4u14@gmail.com>
2021-03-15 19:05:22 -0400

    Fixed html routes to enable moving between pages

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = function (app) {
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-};
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
